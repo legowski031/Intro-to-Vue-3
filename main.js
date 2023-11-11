0ea309c93f4a202d17ec5eafe0911312e288771f
legowski031 <legowski@net.hr>
2023-11-11 09:03:41 +0100

    L2-done

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return{
+            product: 'Socks',
+            description: 'Move along, nothing to see here, just simple Socks.'
+        }
+    }
+})
